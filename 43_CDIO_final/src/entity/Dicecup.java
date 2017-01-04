251124b098d01f1745899ced48f61a0821eb02cd
s145032 <s145032@DTU-CZC6268C6P.win.dtu.dk>
2017-01-04 12:42:08 +0100

    *insert message*

--- 43_CDIO_final/src/entity/Dicecup.java
+++ 43_CDIO_final/src/entity/Dicecup.java
@@ -1,5 +1,36 @@
 package entity;
 
+import entity.Die;
+
+
 public class Dicecup {
 
+	//Variabler
+	private Die x[]; //new Die[2];  
+	private int sum; 
+
+	public Dicecup() {
+		x = new Die[2]; //Antal terninger
+		x[0] = new Die(1);
+		x[1] = new Die(1);
+	}
+ 
+	public void rollDice() { 
+		sum = 0; //reset sum 
+		x[0].rollDie();
+		x[1].rollDie();
+		sum += x[0].getValue(); 
+		sum += x[1].getValue();
+	}
+
+	public int getSum() {
+		return sum;
+	}
+	
+	public Die[] getX() {
+		return x;
+	}
+	
 }
+
+
--- 43_CDIO_final/src/entity/Die.java
+++ 43_CDIO_final/src/entity/Die.java
@@ -2,4 +2,32 @@ package entity;
 
 public class Die {
 
-}
+		//Variabel
+		private int value;
+		
+		//Konstruktoer
+		public Die(int a) {
+			value = a; 
+		}
+		
+		//Getter 
+		public int getValue() {
+			return value;
+		}
+		
+		//Setter 
+		public void setValue(int a) {
+			if (a > 0 && a < 7) {
+				value = a;
+			}
+			else
+				value = 1;
+		}
+		
+		//Tilfaeldig vaerdi
+		public void rollDie() {
+			value = (int) (Math.random() * 6) + 1;  
+		}
+		
+	}
+
