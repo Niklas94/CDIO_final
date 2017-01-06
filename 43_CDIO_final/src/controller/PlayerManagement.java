7998862e3db50a8aa15bec5a931f2095eac007e9
duran <duran@DESKTOP-IHDNLF1>
2017-01-06 01:04:15 +0100

    Made most of the class ready.

--- 43_CDIO_final/src/controller/PlayerManagement.java
+++ 43_CDIO_final/src/controller/PlayerManagement.java
@@ -1,5 +1,66 @@
 package controller;
 
-public class PlayerManagement {
+import entity.Player;
 
+ 
+
+public class PlayerManagement {
+	
+	Player[] player;
+	int playerCount;
+	int playersLeft;
+	
+	//Creates an array of total players.
+	public PlayerManagement(int numberOfPlayers) {
+		player = new Player[numberOfPlayers];
+		this.playerCount = numberOfPlayers;
+	}
+	
+	//Creates the players.
+	public void createPlayers(String name) {
+		for (int i = 0; i < playerCount; i ++) {
+			this.player[i] = new Player(name);
+		}
+	}
+	
+	//Updates total number of players.
+	public void updatePlayerCount(int count) {
+		this.playerCount = count;
+	}
+	
+	//Getter to return player name based on the index number of the player array.
+	public String getPlayerName(int playerIndex) {
+		return player[playerIndex].getName();
+	}
+	
+	//Getter to return players left.
+	public int getPlayersLeft() {
+		return playersLeft;
+	}
+	
+	//Check if player is bankrupt.
+	public void getPlayerStatus(int playerIndex) {
+		player[playerIndex].checkIfBankrupt();
+	}
+	
+	//Updates the position of a player.
+	public void updatePosition(int playerIndex, int position) {
+		player[playerIndex].setPosition(position);
+	}
+	
+	//Getter to return the position of a player
+	public int getPosition(int playerIndex) {
+		return player[playerIndex].getPosition();
+	}
+	
+	/*Code will be done when Board class is finished.
+	public void removePlayerOwnership() {
+		
+	}
+	
+	
+	public void buyField() {
+		
+	}
+	*/
 }
