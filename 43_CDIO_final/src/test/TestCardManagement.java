3d9e538320c605cd4cdd38adc7c694df54348d35
NiklasThielemann <NiklasThielemann@NiklasThieleman>
2017-01-07 14:00:11 +0100

    More methods in CardManagement - created more cards aswell - now all Money and Position orientated are in the game.

--- 43_CDIO_final/src/test/TestCardManagement.java
+++ 43_CDIO_final/src/test/TestCardManagement.java
@@ -15,14 +15,13 @@ public class TestCardManagement {
 
 	@Before
 	public void setUp() throws Exception {
-		cm = new CardManagement(17);
+		cm = new CardManagement(26);
 	}
 
 	@After
 	public void tearDown() throws Exception {
 		cm = null;
 	}
-
 	/*
 
 	I am testing the shuffle cards method, by checking the id of a card at a specific index AFTER shuffling. 
@@ -38,11 +37,10 @@ public class TestCardManagement {
 		cm.createCards();
 		cm.shuffleCards();
 
-		for (int i = 0; i < 17; i++) {
+		for (int i = 0; i < 26; i++) {
 			if (i+1 == cm.pullCard(i).getId()) //I plus i with 1, since each card has an id 1 greater than the index they are located at in the array. Example: card at index 0, has the id 1.
 				actual++;
 		}
-
 		assertEquals(expected, actual);
 	}
 
@@ -56,7 +54,7 @@ public class TestCardManagement {
 		Card expected2 = cm.pullCard(0); //This method returns the card from the specific index. In this test, we obviously pick the first card.
 		Card actual2 = cm.pullTopCard(); //The method were testing - it is meant to return the top card while also moving all cards in the deck, 1 index down. 
 
-		for (int i = 0; i < 17; i++) { //This whole for loop is meant to test if the new cards at each index is the same as we would expect, after pulling the top card from the deck once.
+		for (int i = 0; i < 26; i++) { //This whole for loop is meant to test if the new cards at each index is the same as we would expect, after pulling the top card from the deck once.
 			if (cm.pullCard(i) == null) { //This if-statement is only made to prevent a nullPointerException. Since obviously when you pull a card from a deck, the deck will be 1 card smaller, until the card is returned.
 				System.out.println("Pulled a null card"); //But in our code, we specified the deck as an array which always is the same size, even when you pull a card. In our code, when pulling a card, we make the last index of the array pointing at no object, until A card is returned.
 			}
@@ -73,10 +71,10 @@ public class TestCardManagement {
 	public void testReturnCardToDeck() {
 		
 		cm.createCards();
-		Card expected = cm.pullTopCard();
+		Card expected = cm.pullTopCard(); //Expected card must be the one at the top.
 		cm.returnCardToDeck(cm.getNonOwnableCard(0));
 		
-		Card actual = cm.pullCard(16);
+		Card actual = cm.pullCard(25);
 		assertEquals(expected, actual);
 
 	}
