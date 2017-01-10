73b8ce672af6f95754e2830f12f76292e0239f0f
sammy <sammy@LAPTOP-NMHSVPU8.eduroam.wireless.dtu.dk>
2017-01-10 12:29:20 +0100

    Bank er 90% done.

--- 43_CDIO_final/src/entity/Ownable.java
+++ 43_CDIO_final/src/entity/Ownable.java
@@ -1,5 +1,21 @@
 package entity;
 
-public class Ownable {
+public abstract class Ownable {
+	int price;
+	String name;
+int basevalue;
 
+	public Ownable (String name, int price) {
+		this.price = price;
+		this.name = name;
+
+	}
+	public int getPrice() {
+		return price;
+	}
+	
+	public int getValue() {
+		
+		return basevalue;
+	}
 }
--- 43_CDIO_final/src/entity/Street.java
+++ 43_CDIO_final/src/entity/Street.java
@@ -1,5 +1,23 @@
 package entity;
 
-public class Street {
+public class Street extends Ownable {
+
+	public Street(String name, int price) {
+		super(name,price);
+	}
+
+	@Override
+	public int getPrice() {
+		// TODO Auto-generated method stub
+		return price;
+	}
+
+	@Override
+	public int getValue() {
+		// TODO Auto-generated method stub
+		return basevalue;
+	}
+
 
 }
+
