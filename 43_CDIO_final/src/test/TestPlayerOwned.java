b27be1f8f0b06f3fcbee5df8ffe046b9a0efc759
dukocuk <duran.kse@gmail.com>
2017-01-10 11:33:39 +0100

    done with junit test

--- 43_CDIO_final/src/test/TestPlayerOwned.java
+++ 43_CDIO_final/src/test/TestPlayerOwned.java
@@ -9,272 +9,259 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import desktop_fields.Brewery;
 import entity.PlayerOwned;
 
 public class TestPlayerOwned {
 	
 	PlayerOwned playerOwned;
 
+
+	
+	
 	@Before
 	public void setUp() throws Exception {
 		playerOwned = new PlayerOwned();
-		
 	}
 
 	@After
 	public void tearDown() throws Exception {
 		playerOwned = null;
 	}
-
-	@Test
-	public void testPlayerOwned() {
-		// Testing if variables get initialized with the int value 1
-		int actual = 0;
-		int expected = playerOwned.getTotalBreweryOwned();
-		assertEquals(actual, expected);
-		expected = playerOwned.getTotalFleetOwned();
-		assertEquals(actual, expected);
-		expected = playerOwned.getTotalStreetBlueOwned();
-		assertEquals(actual, expected);
-		expected = playerOwned.getTotalStreetGreenOwned();
-		assertEquals(actual, expected);
-		expected = playerOwned.getTotalStreetGreyOwned();
-		assertEquals(actual, expected);
-		expected = playerOwned.getTotalStreetPinkOwned();
-		assertEquals(actual, expected);
-		expected = playerOwned.getTotalStreetPurpleOwned();
-		assertEquals(actual, expected);
-		expected = playerOwned.getTotalStreetRedOwned();
-		assertEquals(actual, expected);
-		expected = playerOwned.getTotalStreetWhiteOwned();
-		assertEquals(actual, expected);
-		expected = playerOwned.getTotalStreetYellowOwned();
-		assertEquals(actual, expected);
-		expected = playerOwned.getTotalStreetOwned();
-		assertEquals(actual, expected);
 		
-		// Testing if the arrays get initialized with the given value
-		Boolean[] actualArray = {false, false};
-		Boolean[] expectedArray = (Boolean[]) Array.newInstance(Boolean.class, 2);
-		
-		
-		
-		
-	}
 
 	@Test
 	public void testSetBreweryOwned() {
-		fail("Not yet implemented");
+		boolean actual = playerOwned.getBreweryOwned(1);
+		assertFalse(actual);
+		playerOwned.setBreweryOwned(1, true);
+		actual = playerOwned.getBreweryOwned(1);
+		assertTrue(actual);
 	}
 
 	@Test
 	public void testSetFleetOwned() {
-		fail("Not yet implemented");
+		boolean actual = playerOwned.getFleetOwned(1);
+		assertFalse(actual);
+		playerOwned.setFleetOwned(1, true);
+		actual = playerOwned.getFleetOwned(1);
+		assertTrue(actual);
+		
 	}
 
 	@Test
 	public void testSetStreetBlueOwned() {
-		fail("Not yet implemented");
+		boolean actual = playerOwned.getStreetBlueOwned(1);
+		assertFalse(actual);
+		playerOwned.setStreetBlueOwned(1, true);
+		actual = playerOwned.getStreetBlueOwned(1);
+		assertTrue(actual);
 	}
 
 	@Test
 	public void testSetStreetGreenOwned() {
-		fail("Not yet implemented");
+		boolean actual = playerOwned.getStreetGreenOwned(1);
+		assertFalse(actual);
+		playerOwned.setStreetGreenOwned(1, true);
+		actual = playerOwned.getStreetGreenOwned(1);
+		assertTrue(actual);
 	}
 
 	@Test
 	public void testSetStreetGreyOwned() {
-		fail("Not yet implemented");
+		boolean actual = playerOwned.getStreetGreyOwned(1);
+		assertFalse(actual);
+		playerOwned.setStreetGreyOwned(1, true);
+		actual = playerOwned.getStreetGreyOwned(1);
+		assertTrue(actual);
 	}
 
 	@Test
 	public void testSetStreetPinkOwned() {
-		fail("Not yet implemented");
+		boolean actual = playerOwned.getStreetPinkOwned(1);
+		assertFalse(actual);
+		playerOwned.setStreetPinkOwned(1, true);
+		actual = playerOwned.getStreetPinkOwned(1);
+		assertTrue(actual);
 	}
 
 	@Test
 	public void testSetStreetPurpleOwned() {
-		fail("Not yet implemented");
+		boolean actual = playerOwned.getStreetPurpleOwned(1);
+		assertFalse(actual);
+		playerOwned.setStreetPurpleOwned(1, true);
+		actual = playerOwned.getStreetPurpleOwned(1);
+		assertTrue(actual);
 	}
 
 	@Test
 	public void testSetStreetRedOwned() {
-		fail("Not yet implemented");
+		boolean actual = playerOwned.getStreetRedOwned(1);
+		assertFalse(actual);
+		playerOwned.setStreetRedOwned(1, true);
+		actual = playerOwned.getStreetRedOwned(1);
+		assertTrue(actual);
 	}
 
 	@Test
 	public void testSetStreetWhiteOwned() {
-		fail("Not yet implemented");
+		boolean actual = playerOwned.getStreetWhiteOwned(1);
+		assertFalse(actual);
+		playerOwned.setStreetWhiteOwned(1, true);
+		actual = playerOwned.getStreetWhiteOwned(1);
+		assertTrue(actual);
 	}
 
 	@Test
 	public void testSetStreetYellowOwned() {
-		fail("Not yet implemented");
+		boolean actual = playerOwned.getStreetYellowOwned(1);
+		assertFalse(actual);
+		playerOwned.setStreetYellowOwned(1, true);
+		actual = playerOwned.getStreetYellowOwned(1);
+		assertTrue(actual);
 	}
 
 	@Test
 	public void testGetTotalBreweryOwned() {
-		fail("Not yet implemented");
+		int actual = playerOwned.getTotalBreweryOwned();
+		int expected = 0;
+		assertEquals(actual, expected);
 	}
 
 	@Test
 	public void testGetTotalFleetOwned() {
-		fail("Not yet implemented");
+		int actual = playerOwned.getTotalFleetOwned();
+		int expected = 0;
+		assertEquals(actual, expected);
 	}
 
 	@Test
 	public void testGetTotalStreetOwned() {
-		fail("Not yet implemented");
+		int actual = playerOwned.getTotalStreetOwned();
+		int expected = 0;
+		assertEquals(actual, expected);
 	}
 
 	@Test
 	public void testGetTotalStreetBlueOwned() {
-		fail("Not yet implemented");
+		int actual = playerOwned.getTotalStreetBlueOwned();
+		int expected = 0;
+		assertEquals(actual, expected);
 	}
 
 	@Test
 	public void testGetTotalStreetGreenOwned() {
-		fail("Not yet implemented");
+		int actual = playerOwned.getTotalStreetGreenOwned();
+		int expected = 0;
+		assertEquals(actual, expected);
 	}
 
 	@Test
 	public void testGetTotalStreetGreyOwned() {
-		fail("Not yet implemented");
+		int actual = playerOwned.getTotalStreetGreyOwned();
+		int expected = 0;
+		assertEquals(actual, expected);
 	}
 
 	@Test
 	public void testGetTotalStreetPinkOwned() {
-		fail("Not yet implemented");
+		int actual = playerOwned.getTotalStreetPinkOwned();
+		int expected = 0;
+		assertEquals(actual, expected);
 	}
 
 	@Test
 	public void testGetTotalStreetPurpleOwned() {
-		fail("Not yet implemented");
+		int actual = playerOwned.getTotalStreetPurpleOwned();
+		int expected = 0;
+		assertEquals(actual, expected);
 	}
 
 	@Test
 	public void testGetTotalStreetRedOwned() {
-		fail("Not yet implemented");
+		int actual = playerOwned.getTotalStreetRedOwned();
+		int expected = 0;
+		assertEquals(actual, expected);
 	}
 
 	@Test
 	public void testGetTotalStreetWhiteOwned() {
-		fail("Not yet implemented");
+		int actual = playerOwned.getTotalStreetWhiteOwned();
+		int expected = 0;
+		assertEquals(actual, expected);
 	}
 
 	@Test
 	public void testGetTotalStreetYellowOwned() {
-		fail("Not yet implemented");
+		int actual = playerOwned.getTotalStreetYellowOwned();
+		int expected = 0;
+		assertEquals(actual, expected);
 	}
 
 	@Test
 	public void testGetBreweryOwned() {
-		fail("Not yet implemented");
+		boolean actual = playerOwned.getBreweryOwned(1);
+		assertFalse(actual);
 	}
 
 	@Test
 	public void testGetFleetOwned() {
-		fail("Not yet implemented");
+		boolean actual = playerOwned.getFleetOwned(1);
+		assertFalse(actual);
 	}
 
 	@Test
 	public void testGetStreetBlueOwned() {
-		fail("Not yet implemented");
+		boolean actual = playerOwned.getStreetBlueOwned(1);
+		assertFalse(actual);
 	}
 
 	@Test
 	public void testGetStreetGreenOwned() {
-		fail("Not yet implemented");
+		boolean actual = playerOwned.getStreetGreenOwned(1);
+		assertFalse(actual);
 	}
 
 	@Test
 	public void testGetStreetGreyOwned() {
-		fail("Not yet implemented");
+		boolean actual = playerOwned.getStreetGreyOwned(1);
+		assertFalse(actual);
 	}
 
 	@Test
 	public void testGetStreetPinkOwned() {
-		fail("Not yet implemented");
+		boolean actual = playerOwned.getStreetBlueOwned(1);
+		assertFalse(actual);
 	}
 
 	@Test
 	public void testGetStreetPurpleOwned() {
-		fail("Not yet implemented");
+		boolean actual = playerOwned.getStreetPurpleOwned(1);
+		assertFalse(actual);
 	}
 
 	@Test
 	public void testGetStreetRedOwned() {
-		fail("Not yet implemented");
+		boolean actual = playerOwned.getStreetRedOwned(1);
+		assertFalse(actual);
 	}
 
 	@Test
 	public void testGetStreetWhiteOwned() {
-		fail("Not yet implemented");
+		boolean actual = playerOwned.getStreetWhiteOwned(1);
+		assertFalse(actual);
 	}
 
 	@Test
 	public void testGetStreetYellowOwned() {
-		fail("Not yet implemented");
+		boolean actual = playerOwned.getStreetYellowOwned(1);
+		assertFalse(actual);
 	}
 
 	@Test
 	public void testObject() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetClass() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testHashCode() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testEquals() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testClone() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testToString() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testNotify() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testNotifyAll() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testWaitLong() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testWaitLongInt() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testWait() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testFinalize() {
-		fail("Not yet implemented");
+		assertNotNull(playerOwned);
+		assertEquals(playerOwned.getClass(), PlayerOwned.class);
 	}
 
 }
