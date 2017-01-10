255fe88ae590638015592a37c045b4f308d89fc2
dukocuk <duran.kse@gmail.com>
2017-01-10 18:37:54 +0100

    coded some GUI text

--- 43_CDIO_final/src/boundary/GUI.java
+++ 43_CDIO_final/src/boundary/GUI.java
@@ -1,57 +1,41 @@
 package boundary;
 
-import entity.Player;
-
 public class GUI {
-	
 
-	
 	private static int numberPlayers;
 	private static String name;
 
 	public static int numberOfPlayers() {
-		numberPlayers = GUI.getUserInteger("Indtast antal spillere" + " mellem 2 og 6.");
+		numberPlayers = desktop_resources.GUI.getUserInteger("Indtast antal spillere" + " mellem 3 og 6.");
 		return numberPlayers;
 	}
 
 	public static String playerNames() {
-
-		name = GUI.getUserString("Hvad er dit navn?");
+		name = desktop_resources.GUI.getUserString("Hvad er dit navn?");
 		return name;
-
 	}
-	
-	public static void getPlayerNames(String name) {
 
-		GUI.showMessage(name);
-		
+	public static void getPlayerNames(String name) {
+		desktop_resources.GUI.showMessage(name);
 	}
 
 	public static void winnerFound(String name) {
 
-		GUI.showMessage("Vinderen er: " + name);
+		desktop_resources.GUI.showMessage("Vinderen er: " + name);
 
-				
 	}
-	
+
 	public static void pressEnter(String name) {
-		
-		GUI.showMessage(name + " tryk enter for at kaste med terningerne");
 
-		
+		desktop_resources.GUI.showMessage(name + " tryk enter for at kaste med terningerne");
+
 	}
-	
+
 	public static void printValue(int sum) {
-		
-		GUI.showMessage("Du har kastet: " + sum);
 
-		
-	}
-	
-	public static void printBalance(Player player) {
-		
-//		GUI.showMessage(player.getName() + ", din pengebeholdning er nu på " + player.getAccount().getMoney());
+		desktop_resources.GUI.showMessage("Du har kastet: " + sum);
 
 	}
+
 
 }
