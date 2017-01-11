dc6549b6360f88138283e0f220345ac63f23badb
s143775 <s143775@DTU-CZC6268C62.win.dtu.dk>
2017-01-11 17:50:46 +0100

    CardManagement change

--- 43_CDIO_final/src/controller/CardManagement.java
+++ 43_CDIO_final/src/controller/CardManagement.java
@@ -15,7 +15,7 @@ import java.io.BufferedReader;
 
 public class CardManagement {
 
-	private Card[] cardArr = new Card[31];
+	private Card[] cardArr = new Card[31]; //Change the number to create more cards - remember to add type, value and such in "Card Description.txt" file
 	private Player[] players;
 
 	//Card Array
