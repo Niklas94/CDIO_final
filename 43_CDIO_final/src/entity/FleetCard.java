3c71e409fac5d6aa90b1af76ce7705ecaafab9e0
s143775 <s143775@DTU-CZC6268C60.win.dtu.dk>
2017-01-15 19:42:36 +0100

    Rettelse i fleetcard

--- 43_CDIO_final/src/entity/FleetCard.java
+++ 43_CDIO_final/src/entity/FleetCard.java
@@ -6,7 +6,7 @@ public class FleetCard extends Card {
 
 	private Bank bank;
 	private BoardManagement bm;
-	
+
 	public FleetCard(String desc, Bank bank, BoardManagement bm) {
 		super.setDescription(desc);
 		this.bank = bank;
@@ -19,7 +19,10 @@ public class FleetCard extends Card {
 		if (player.getPosition() >= 3 && player.getPosition() <= 8) {
 			player.setPosition(6);
 			if (bm.returnFleet(player.getPosition()).isOwned && bm.returnFleet(player.getPosition()).getOwner() != player.getName()) {
-				bank.payRent(player, bm.returnFleet(player.getPosition()).getOwner(), bm.returnFleet(player.getPosition()).getRent() * 2);
+				if (this.getDescription().equals("Tag med den nærmeste færge - Flyt brikken frem, og hvis De passerer Start inkassér da kr. 4.000."))
+					bank.payRent(player, bm.returnFleet(player.getPosition()).getOwner(), bm.returnFleet(player.getPosition()).getRent());
+				else
+					bank.payRent(player, bm.returnFleet(player.getPosition()).getOwner(), bm.returnFleet(player.getPosition()).getRent() * 2);
 			}
 			else if (!bm.returnFleet(player.getPosition()).isOwned) {
 				bm.buyField(player, bm.returnFleet(player.getPosition()).getPrice(), "Fleet");
@@ -29,7 +32,10 @@ public class FleetCard extends Card {
 		{
 			player.setPosition(16);
 			if (bm.returnFleet(player.getPosition()).isOwned && bm.returnFleet(player.getPosition()).getOwner() != player.getName()) {
-				bank.payRent(player, bm.returnFleet(player.getPosition()).getOwner(), bm.returnFleet(player.getPosition()).getRent() * 2);
+				if (this.getDescription().equals("Tag med den nærmeste færge - Flyt brikken frem, og hvis De passerer Start inkassér da kr. 4.000."))
+					bank.payRent(player, bm.returnFleet(player.getPosition()).getOwner(), bm.returnFleet(player.getPosition()).getRent());
+				else
+					bank.payRent(player, bm.returnFleet(player.getPosition()).getOwner(), bm.returnFleet(player.getPosition()).getRent() * 2);
 			}
 			else if (!bm.returnFleet(player.getPosition()).isOwned) {
 				bm.buyField(player, bm.returnFleet(player.getPosition()).getPrice(), "Fleet");
@@ -38,7 +44,10 @@ public class FleetCard extends Card {
 		else if (player.getPosition() == 23) {
 			player.setPosition(26);
 			if (bm.returnFleet(player.getPosition()).isOwned && bm.returnFleet(player.getPosition()).getOwner() != player.getName()) {
-				bank.payRent(player, bm.returnFleet(player.getPosition()).getOwner(), bm.returnFleet(player.getPosition()).getRent() * 2);
+				if (this.getDescription().equals("Tag med den nærmeste færge - Flyt brikken frem, og hvis De passerer Start inkassér da kr. 4.000."))
+					bank.payRent(player, bm.returnFleet(player.getPosition()).getOwner(), bm.returnFleet(player.getPosition()).getRent());
+				else
+					bank.payRent(player, bm.returnFleet(player.getPosition()).getOwner(), bm.returnFleet(player.getPosition()).getRent() * 2);
 			}
 			else if (!bm.returnFleet(player.getPosition()).isOwned) {
 				bm.buyField(player, bm.returnFleet(player.getPosition()).getPrice(), "Fleet");
@@ -47,7 +56,10 @@ public class FleetCard extends Card {
 		else if (player.getPosition() >= 34 && player.getPosition() <= 37) {
 			player.setPosition(36);	
 			if (bm.returnFleet(player.getPosition()).isOwned && bm.returnFleet(player.getPosition()).getOwner() != player.getName()) {
-				bank.payRent(player, bm.returnFleet(player.getPosition()).getOwner(), bm.returnFleet(player.getPosition()).getRent() * 2);
+				if (this.getDescription().equals("Tag med den nærmeste færge - Flyt brikken frem, og hvis De passerer Start inkassér da kr. 4.000."))
+					bank.payRent(player, bm.returnFleet(player.getPosition()).getOwner(), bm.returnFleet(player.getPosition()).getRent());
+				else
+					bank.payRent(player, bm.returnFleet(player.getPosition()).getOwner(), bm.returnFleet(player.getPosition()).getRent() * 2);
 			}
 			else if (!bm.returnFleet(player.getPosition()).isOwned) {
 				bm.buyField(player, bm.returnFleet(player.getPosition()).getPrice(), "Fleet");
