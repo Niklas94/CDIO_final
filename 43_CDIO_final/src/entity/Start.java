18d239076f73bae90909e76ba71919d6e20be373
s147153 <s147153@DTU-CZC6268C8F.win.dtu.dk>
2017-01-15 20:05:20 +0100

    javadoc

--- 43_CDIO_final/src/entity/Start.java
+++ 43_CDIO_final/src/entity/Start.java
@@ -2,16 +2,31 @@ package entity;
 
 public class Start extends NonOwnable {
 
+	/**
+	 * This constructs the start field with a id, name and type.
+	 * @param id id of the square.
+	 * @param name name of the square.
+	 * @param type type of the square.
+	 */
+	
 	public Start(int id, String name, String type) {
 		super(id);
 		this.name = name;
 		this.type = type;
 	}
 
+	/**
+	 * sets the land on field.
+	 */
+	
 	@Override
 	public void landOnField(Player player) {
 		
 	}
+	
+	/**
+	 * returns the id of the field.
+	 */
 
 	@Override
 	public int getId() {
