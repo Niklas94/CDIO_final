5d6ad0514d839894e20c1392d394a5c36acd079e
s147153 <s147153@DTU-CZC6268C8F.win.dtu.dk>
2017-01-15 21:16:38 +0100

    javadoc

--- 43_CDIO_final/src/entity/Board.java
+++ 43_CDIO_final/src/entity/Board.java
@@ -26,8 +26,9 @@ public class Board {
 	public Square[] logicField;
 	private int blue, green, grey, pink, purple, red, white, yellow;
 
+	
 	/**
-	 * 
+	 * This constructs the board.
 	 */
 	
 	public Board(){
@@ -37,9 +38,19 @@ public class Board {
 			logicField = new Square[40];
 	}
 	
+	/**
+	 * @return returns the logicFields.
+	 */
+	
 	public Square[] getLogicField(){
 		return logicField;
 	}
+	
+	/**
+	 * 
+	 * @param id id of the square id.
+	 * @return returns the ligicField as ownable.
+	 */
 
 	public Ownable getOwnableSquare(int id){
 		Ownable o = (Ownable) logicField[id];
@@ -47,10 +58,24 @@ public class Board {
 	}
 
 	
+	/**
+	 * 
+	 * @param id id of the square.
+	 * @return returns the logicField.
+	 */
+	
 	public Square getSquare(int id) {
 		return logicField[id];
 	}
 
+	
+	/**
+	 * 
+	 * @param cm the controlling cardManagement.
+	 * @param cup the diceCup.
+	 * @param bm the controlling boardManagement.
+	 */
+	
 	public void createBoard(CardManagement cm, Dicecup cup, BoardManagement bm) {
 		FileReader file;
 		try {
@@ -115,6 +140,12 @@ public class Board {
 
 
 	}
+	
+	/**
+	 * 
+	 * @param values the values.
+	 * @param index the index.
+	 */
 
 	public void createStart(String[] values, int index){
 		Color bgColor = getColor(getValue("backgroundColor", values));
@@ -133,6 +164,13 @@ public class Board {
 		logicField[index] = new entity.Start(index + 1, title, "Start");
 
 	}
+	
+	/**
+	 * 
+	 * @param values the values.
+	 * @param index the index.
+	 * @param bm the controlling boardManagement.
+	 */
 
 	public void createStreet(String[] values, int index, BoardManagement bm){
 		String title = getValue("title", values);
@@ -190,6 +228,14 @@ public class Board {
 		}
 //		logicField[index] = new entity.Street(index + 1, title, price, rentForLogic, bm, "Street");
 	}
+	
+	/**
+	 * 
+	 * @param values the values.
+	 * @param index the index.
+	 * @param cup the diceCup.
+	 * @param bm the controlling boardManagement.
+	 */
 
 	public void createBrewery(String[] values, int index, Dicecup cup, BoardManagement bm){
 		String picture;
@@ -216,6 +262,12 @@ public class Board {
 
 	}
 
+	/**
+	 * 
+	 * @param values the values.
+	 * @param index the index.
+	 * @param bm the controlling boardManagement.
+	 */
 	public void createFleet(String[] values, int index, BoardManagement bm){
 		String title = getValue("title", values);
 		String subText = getValue("subText", values);
@@ -233,6 +285,13 @@ public class Board {
 		logicField[index] = new entity.Fleet(index + 1, title, price, rentForLogic, bm, "Fleet");
 
 	}
+	
+	/**
+	 * creates the parking.
+	 * @param values the values.
+	 * @param index the index.
+	 * @param bm the controlling boardManagement.
+	 */
 
 	public void createParking(String[] values, int index, BoardManagement bm){
 		String title = getValue("title", values);
@@ -247,6 +306,15 @@ public class Board {
 		logicField[index] = new entity.BankParking(index + 1, title, bm, "Parking");
 
 	}
+	
+	/**
+	 * creates the jail.
+	 * @param values the values.
+	 * @param index the inedx.
+	 * @param cup the diceCup.
+	 * @param cm the controlling cardManagement.
+	 * @param bm the controlling boardManagement.
+	 */
 
 	public void createJail(String[] values, int index, Dicecup cup, CardManagement cm, BoardManagement bm){
 		String title = getValue("title", values);
