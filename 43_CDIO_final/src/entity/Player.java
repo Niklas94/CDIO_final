39666289ca6eb7ab06699180b2f3745fb7b8c956
s147153 <s147153@DTU-CZC6268C8F.win.dtu.dk>
2017-01-15 20:01:29 +0100

    javadoc

--- 43_CDIO_final/src/entity/Player.java
+++ 43_CDIO_final/src/entity/Player.java
@@ -49,6 +49,10 @@ public class Player {
 		}
 	}
 
+	/**
+	 * changes the boolean value of inJail.
+	 */
+	
 	public void inJail() {
 		if (!inJail)
 			this.inJail = true;
@@ -56,16 +60,27 @@ public class Player {
 			this.inJail = false;
 	}
 
+	/**
+	 * increments incGoToJail.
+	 */
 	public void incGoToJail() {
 		this.goToJail++;
 	}
 
+	/**
+	 * resets goToJail.
+	 */
 
 	public void resetGoToJail() {
 		this.goToJail = 0;
 	}
 
 	// Set jailcard to owned
+	
+	/**
+	 * gives jailCard to player.
+	 * @param card the type of card.
+	 */
 	public void setJailCardOwned(Card card) {
 		for (int i = 0; i < jailCards.length; i++) {
 			if (jailCards[i] == null) {
@@ -78,6 +93,7 @@ public class Player {
 	/**
 	 * removes a Get-Out-Of-Jail-Card from the player.
 	 */
+	
 	public void useJailCardOwned() {
 		for (int i = 0; i < jailCards.length; i++) {
 			if (jailCards[i] != null) {
@@ -89,7 +105,7 @@ public class Player {
 
 	/**
 	 * Method to update a given player's balance.
-	 * @param money
+	 * @param money the amount of money.
 	 */
 	public void updateBalance(int money) {
 		this.myAccount.updateBalance(money);
@@ -111,11 +127,22 @@ public class Player {
 			this.isAlive = false;
 		return isAlive;
 	}
+	
+	/**
+	 * sets the position of a player.
+	 * @param position position of the player.
+	 */
 
 	// Set player position
 	public void setPosition(int position) {
 		this.position = position;
 	}
+	
+	/**
+	 * 
+	 * @param breweryNumber
+	 * @param bool
+	 */
 
 	// Set the Brewery to owned or not by its number and a boolean value.
 	public void setBreweryOwned(int breweryNumber, Boolean bool) {
